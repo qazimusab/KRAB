77d3d5eea5277e5a63cb916becba38c3fba3e842
qaziahmed <qazi.ahmed@ncr.com>
2015-11-13 17:51:45 -0500

    Added new vision.

--- js/default.js
+++ js/default.js
@@ -1,15 +1,7 @@
 /**
  * Created by Fabrice on 10/16/15.
  */
-var locationSuccessDialog = document.getElementById("successDialogWindow");
-var locationErrorDialog = document.getElementById("errorDialogWindow");
-var locationErrorDialogContent = document.getElementById("errorDialogContent");
-var zipCodeTextBox = document.getElementById("errorDialogZipCode");
-var manualZip = document.getElementById("manualZipContainer");
 var container = document.getElementById("container");
-var jumbotron = document.getElementById("jumbotron");
-var sitesList = document.getElementById("sitesList");
-var manualZipInput = document.getElementById("manualZipInput");
 var spinner;
 var opts = {
     lines: 13 // The number of lines to draw
@@ -33,68 +25,26 @@ var opts = {
     , hwaccel: false // Whether to use hardware acceleration
     , position: 'absolute' // Element positioning
 };
-var mapCanvas = document.getElementById('map');
-
-var SEARCH_RANGE = 10;
 
 var latitude;
 var longitude;
-var zipCode;
 
-var siteMap;
-var geoCoder = new google.maps.Geocoder();
-
-
-function initializeMap() {
-
-    var mapOptions = {
-        center: {lat: 33.7550, lng: -84.39},
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    siteMap = new google.maps.Map(mapCanvas, mapOptions);
-}
+$(document).ready(function() {
+    spinner = new Spinner(opts).spin(container);
+    getLocation();
+});
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
-    } else {
-        locationErrorDialog.show();
-    }
-}
-
-function getDistanceFromLatLonInMiles(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1);
-    var a =
-            Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2)
-        ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in km
-    var distanceInMiles = d * 0.621371;
-    return distanceInMiles;
-}
-
-function deg2rad(deg) {
-    return deg * (Math.PI/180)
+    navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
 }
 
 function onLocationSuccess(position) {
-    successDialogContent.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
-
-    locationSuccessDialog.show();
-
+    loadMenu();
 }
 
 function onLocationError(error) {
-    manualZip.style.display = 'block';
 
     switch(error.code) {
         case error.PERMISSION_DENIED:
@@ -117,172 +67,32 @@ function onLocationError(error) {
 
 }
 
-function dismissSuccessDialog(){
-    locationSuccessDialog.close();
-    showSitesPage();
-}
-
-function showSitesPage() {
-    google.maps.event.addDomListener(window, 'load', initializeMap());
-    jumbotron.style.display = 'none';
-    manualZip.style.display = 'none';
-    spinner = new Spinner(opts).spin(container);
-    tabsContainer.style.display = 'initial';
-    sitesList.style.display = 'initial';
-    mapCanvas.style.display = 'none';
-
-
-    siteMap.setCenter(new google.maps.LatLng(latitude, longitude));
-    $.getJSON("js/sites.json", function(json) {
-        var unsortedSitesList = [];
-        var sheet1 = json["Sheet1"];
-        for(var i = 0; i < sheet1.length; i++){
-            var site = new Site();
-            site.setSiteId(sheet1[i]["SiteId"]);
-            site.setName(sheet1[i]["Name"]);
-            site.setAddressLine1(sheet1[i]["AddressLine1"]);
-            site.setAddressLine2(sheet1[i]["AddressLine2"]);
-            site.setCity(sheet1[i]["City"]);
-            site.setState(sheet1[i]["State"]);
-            site.setPostal(sheet1[i]["Postal"]);
-            site.setVoicePhone(sheet1[i]["VoicePhone"]);
-            site.setLatitude(sheet1[i]["Latitude"]);
-            site.setLongitude(sheet1[i]["Longitude"]);
-            var distanceInMiles = getDistanceFromLatLonInMiles(latitude, longitude, site.lat, site.long);
-            site.setDistanceFromCurrentLocation(distanceInMiles);
-            unsortedSitesList.push(site)
-        }
-        var sortedSitesList = mergeSort(unsortedSitesList);
-        for(var i = 0; i < sortedSitesList.length && sortedSitesList[i].distanceFromCurrentLocation <= SEARCH_RANGE; i++) {
-            var currentSite = sortedSitesList[i];
-            var distance = currentSite.distanceFromCurrentLocation + "";
-            distance = distance.substring(0, distance.indexOf(".") + 2);
-            $("#tabsContainer ul").append("<a href=\"https://www.google.com/maps/preview/?q="
-                + currentSite.lat + "," + currentSite.long +"\"><li class=\"list-group-item\"> <span class=\"badge\">" +
-                distance + "mi</span>" + currentSite.name + "</li></a>");
-
-            var marker = new google.maps.Marker({
-                position: {lat: Number(sortedSitesList[i].lat), lng: Number(sortedSitesList[i].long)},
-                map: siteMap,
-                title: sortedSitesList[i].name
-            });
-        }
+function loadMenu() {
+    var site = new Site();
+    $.getJSON("js/sites.json",function(json) {
+        site.setSiteId(json[0]["SiteId"]);
+        site.setName(json[0]["Name"]);
+        site.setAddressLine1(json[0]["AddressLine1"]);
+        site.setAddressLine2(json[0]["AddressLine2"]);
+        site.setCity(json[0]["City"]);
+        site.setState(json[0]["State"]);
+        site.setPostal(json[0]["Postal"]);
+        site.setVoicePhone(json[0]["VoicePhone"]);
+        site.setLatitude(json[0]["Latitude"]);
+        site.setLongitude(json[0]["Longitude"]);
+        site.setMenuUrl(json[0]["MenuImage"]);
+        $('#menu_image').attr("src", site.menuUrl);
         spinner.stop()
     });
-
-}
-
-function handleMapToggleClick(){
-    sitesList.style.display = 'none';
-    mapCanvas.style.display = 'block';
-    google.maps.event.trigger(map, 'resize');
-    siteMap.setCenter(new google.maps.LatLng(latitude, longitude));
-}
-
-function handleListToggleClick(){
-    sitesList.style.display = 'initial';
-    mapCanvas.style.display = 'none';
-}
-
-function dismissErrorDialog() {
-    addManualLocationInput();
-    locationErrorDialog.close();
-    showSitesPage();
-}
-
-function addManualLocationInput() {
-    zipCode = zipCodeTextBox.value;
-    convertZipToLatLon();
-}
-
-function convertZipToLatLon() {
-    geoCoder.geocode( {'address' : zipCode}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            latitude = results[0].geometry.location.lat();
-            longitude = results[0].geometry.location.lng();
-        } else {
-            alert("Something went wrong: " + status)
-        }
-    });
-}
-
-function addMapPins(results) {
-    //siteMap.data.addGeoJson(results);
 }
 
 function Site() {
 
-    this.setSiteId = function(siteId){
-        this.siteId = siteId
-    };
-
     this.setName = function(name){
         this.name = name
     };
-
-    this.setAddressLine1 = function(addressLine1){
-        this.addressLine1 = addressLine1
+    this.setMenuUrl = function(menuUrl){
+        this.menuUrl = menuUrl
     };
 
-    this.setAddressLine2 = function(addressLine2){
-        this.addressLine2 = addressLine2
-    };
-
-    this.setCity = function(city){
-        this.city = city
-    };
-
-    this.setState = function(state){
-        this.state = state
-    };
-
-    this.setPostal = function(postal){
-        this.postal = postal
-    };
-
-    this.setVoicePhone = function(voicePhone){
-        this.voicePhone = voicePhone
-    };
-
-    this.setLatitude = function(lat){
-        this.lat = lat
-    };
-
-    this.setLongitude = function(long){
-        this.long = long
-    };
-
-    this.setDistanceFromCurrentLocation = function(distanceFromCurrentLocation){
-        this.distanceFromCurrentLocation = distanceFromCurrentLocation;
-    };
-}
-
-
-function merge(left, right){
-    var result  = [],
-        il      = 0,
-        ir      = 0;
-
-    while (il < left.length && ir < right.length){
-        if (left[il].distanceFromCurrentLocation < right[ir].distanceFromCurrentLocation){
-            result.push(left[il++]);
-        } else {
-            result.push(right[ir++]);
-        }
-    }
-
-    return result.concat(left.slice(il)).concat(right.slice(ir));
-}
-
-function mergeSort(items){
-
-    if (items.length < 2) {
-        return items;
-    }
-
-    var middle = Math.floor(items.length / 2),
-        left    = items.slice(0, middle),
-        right   = items.slice(middle);
-
-    return merge(mergeSort(left), mergeSort(right));
 }
